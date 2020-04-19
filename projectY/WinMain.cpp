6c6c52f23981f8235e1157ec7e320eb5949cc3c8
Adam <adamdlsoelter@gmail.com>
2020-04-18 22:27:17 -0500

    added colored triangle

--- projectY/WinMain.cpp
+++ projectY/WinMain.cpp
@@ -12,192 +12,152 @@
 
 #include <d3d11.h>
 
+#define GFX_THROW_INFO(func) func
+#define GFX_THROW_INFO_ONLY(func) func
+
 struct Vertex
 {
-    DirectX::XMFLOAT4 position;
+    DirectX::XMFLOAT2 position;
     DirectX::XMFLOAT3 color;
 };
 
-#define BREAK_ON_FAIL(res) assert(!FAILED(res));
+static constexpr auto width = 800u;
+static constexpr auto height = 600u;
 
-int WINAPI WinMain(_In_     HINSTANCE hInstance, 
-                   _In_opt_ HINSTANCE hPrevInstance,
-                   _In_     PSTR szCmdLine, 
-                   _In_     int iCmdShow)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-#if defined(DEBUG) | defined(_DEBUG)
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = "wndClass";
+    wc.hIconSm = nullptr;
+    RegisterClassEx(&wc);
 
-    auto window = gui::Window(1000, 750, "Main window");
-
-    gui::Button::Descriptor desc;
-    desc.x = 10;
-    desc.y = 20;
-    desc.width = 100;
-    desc.height = 20;
-    desc.title = "Hide Cursor";
-
-    auto button = gui::Button(desc);
-
-    BOOL showCursor = FALSE;
-
-    button.pushed.connect([&showCursor]() {ShowCursor(showCursor); showCursor = !showCursor; });
-
-    window.attach(std::move(button));
-
-    DXGI_MODE_DESC modeDescription;
-
-    modeDescription.Width = static_cast<UINT>(window.width());
-    modeDescription.Height = static_cast<UINT>(window.height());
-    modeDescription.RefreshRate = { 60, 1 }; //60 frames per second
-    modeDescription.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    modeDescription.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-    modeDescription.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-
-    DXGI_SAMPLE_DESC sampleDescription;
-
-    //default quality
-    sampleDescription.Count = 1;
-    sampleDescription.Quality = 0; 
-
-    DXGI_SWAP_CHAIN_DESC swapDescription;
-
-    swapDescription.BufferDesc = modeDescription;
-    swapDescription.SampleDesc = sampleDescription;
-    swapDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; //this is for screen output
-    swapDescription.BufferCount = 1; //one back buffer
-    swapDescription.OutputWindow = window.handle();
-    swapDescription.Windowed = true;
-    swapDescription.SwapEffect = DXGI_SWAP_EFFECT_DISCARD; //discard back buffer after use
-    swapDescription.Flags = 0;
-
-    //TODO(asoelter): move to ComPtr's for pointer types
-    D3D_FEATURE_LEVEL featureLevels[] = {D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0};
-    ID3D11Device* device;
-    D3D_FEATURE_LEVEL selectedFeatureLevel;
-    ID3D11DeviceContext* context;
-    IDXGISwapChain* swapchain;
-
-    auto flags = static_cast<UINT>(D3D11_CREATE_DEVICE_SINGLETHREADED);
-
-#ifdef DEBUG
-    flags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-
-    auto result = D3D11CreateDeviceAndSwapChain(
-        nullptr, 
-        D3D_DRIVER_TYPE_HARDWARE, 
-        nullptr, 
-        flags,  
-        featureLevels, 
-        2, 
-        D3D11_SDK_VERSION, 
-        &swapDescription, 
-        &swapchain, 
-        &device,
-        &selectedFeatureLevel,
-        &context
-    );
-
-    BREAK_ON_FAIL(result);
-
-    //This basically allows Direct3D to 
-    //draw to the backbuffer
-    ID3D11RenderTargetView* renderView;
-    Microsoft::WRL::ComPtr<ID3D11Texture2D> backBuffer;
-    result = swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), &backBuffer);
-    BREAK_ON_FAIL(result);
-
-    if (!backBuffer)
+    RECT wr;
+    wr.left = 100;
+    wr.right = width + wr.left;
+    wr.top = 100;
+    wr.bottom = height + wr.top;
+    if (AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE) == 0)
     {
-        MessageBox(NULL, "unable to make backbuffer", "unable to make backbuffer", 0);
-        return 0;
+        assert(false);
     }
+    // create window & get hWnd
+    HWND hwnd = CreateWindow(
+        "wndClass", "Window",
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
+        nullptr, nullptr, hInstance, nullptr
+    );
+    // check for error
+    if (!hwnd)
+    {
+        assert(false);
+    }
+    // newly created windows start off as hidden
+    ShowWindow(hwnd, SW_SHOWDEFAULT);
+    // create graphics object
 
-    result = device->CreateRenderTargetView(backBuffer.Get(), nullptr, &renderView);
-    BREAK_ON_FAIL(result);
+    DXGI_SWAP_CHAIN_DESC sd = {};
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 0;
+    sd.BufferDesc.RefreshRate.Denominator = 0;
+    sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+    sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.BufferCount = 1;
+    sd.OutputWindow = hwnd;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    sd.Flags = 0;
 
-    D3D11_TEXTURE2D_DESC depthBufferDesc;
+    UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+    swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
-    depthBufferDesc.Width = static_cast<UINT>(window.width());
-    depthBufferDesc.Height = static_cast<UINT>(window.height());
-    depthBufferDesc.MipLevels = 1;
-    depthBufferDesc.ArraySize = 1;
-    depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-    depthBufferDesc.SampleDesc = sampleDescription;
-    depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-    depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-    depthBufferDesc.CPUAccessFlags = NULL;
-    depthBufferDesc.MiscFlags = NULL;
+    // for checking results of d3d functions
+    HRESULT hr;
 
-    ID3D11Texture2D* depthBuffer;
-    ID3D11DepthStencilView* depthView;
+    Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> pSwap;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
 
-    result = device->CreateTexture2D(
-        &depthBufferDesc, // Description of texture to create.
+    // create device and front/back buffers, and swap chain and rendering context
+    D3D11CreateDeviceAndSwapChain(
         nullptr,
-        &depthBuffer); // Return pointer to depth/stencil buffer.
-
-    BREAK_ON_FAIL(result);
-    assert(depthBuffer);
-
-    result = device->CreateDepthStencilView(
-        depthBuffer, // Resource we want to create a view to.
+        D3D_DRIVER_TYPE_HARDWARE,
         nullptr,
-        &depthView); // Return depth/stencil view
+        swapCreateFlags,
+        nullptr,
+        0,
+        D3D11_SDK_VERSION,
+        &sd,
+        &pSwap,
+        &pDevice,
+        nullptr,
+        &pContext
+    );
+    // gain access to texture subresource in swap chain (back buffer)
+    Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer;
+    pSwap->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
+    pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pTarget);
 
-    BREAK_ON_FAIL(result);
+    const float red = 0.2f;
+    const float green = 0.3f;
+    const float blue = 0.4f;
 
-    context->OMSetRenderTargets(1, &renderView, depthView);
-
-    D3D11_VIEWPORT viewport;
-
-    viewport.TopLeftX = 0.0f;
-    viewport.TopLeftY = 0.0f;
-    viewport.Width    = static_cast<UINT>(window.width());
-    viewport.Height   = static_cast<UINT>(window.height());
-    viewport.MinDepth = 0.0f;
-    viewport.MaxDepth = 1.0f;
-
-    context->RSSetViewports(1, &viewport);
-
-    Vertex vertices[3] = {
-        {DirectX::XMFLOAT4( 0.0f,  0.5f, 0.5f, 1.0f), DirectX::XMFLOAT3(1.0f, 0.0f, 0.0f)},
-        {DirectX::XMFLOAT4( 0.5f, -0.5f, 0.5f, 1.0f), DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f)},
-        {DirectX::XMFLOAT4(-0.5f, -0.5f, 0.5f, 1.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 1.0f)}
+    /////////////////////////////////
+    //create and bind vertex buffer//
+    /////////////////////////////////
+    const Vertex vertices[] =
+    {
+        DirectX::XMFLOAT2{ 0.0f, 0.5f}, DirectX::XMFLOAT3{1.0f,0.0f,0.0f},
+        DirectX::XMFLOAT2{ 0.5f,-0.5f}, DirectX::XMFLOAT3{0.0f,1.0f,0.0f},
+        DirectX::XMFLOAT2{-0.5f,-0.5f}, DirectX::XMFLOAT3{0.0f,0.0f,1.0f}
     };
 
-    D3D11_INPUT_ELEMENT_DESC dataDescription[2] = {
-        {"POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0}
-    };
+    // create vertex buffer (1 2d triangle at center of screen)
+    Microsoft::WRL::ComPtr<ID3D11Buffer> pVertexBuffer;
+    D3D11_BUFFER_DESC bd = {};
+    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    bd.Usage = D3D11_USAGE_DEFAULT;
+    bd.CPUAccessFlags = 0u;
+    bd.MiscFlags = 0u;
+    bd.ByteWidth = sizeof(vertices);
+    bd.StructureByteStride = sizeof(Vertex);
+    D3D11_SUBRESOURCE_DATA srd = {};
+    srd.pSysMem = vertices;
+    GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &srd, &pVertexBuffer));
 
-    D3D11_BUFFER_DESC vbDesc = {};
+    const UINT stride = sizeof(Vertex);
+    const UINT offset = 0u;
+    pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
 
-    vbDesc.ByteWidth = sizeof(vertices);
-    vbDesc.Usage = D3D11_USAGE_DEFAULT;
-    vbDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-    vbDesc.CPUAccessFlags = 0;
-    vbDesc.MiscFlags = 0;
-    vbDesc.StructureByteStride = sizeof(Vertex);
-
-    D3D11_SUBRESOURCE_DATA srData = {};
-
-    srData.pSysMem = vertices;
-
-    Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
-    result = device->CreateBuffer(&vbDesc, &srData, &vertexBuffer);
-    BREAK_ON_FAIL(result);
-
-    UINT stride = sizeof(Vertex);
-    UINT offset = 0;
-    context->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
+    ////////////////////////////////
+    //Create and bind pixel shader//
+    ////////////////////////////////
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> pPixelShader;
 
     Microsoft::WRL::ComPtr<ID3DBlob> blob;
     Microsoft::WRL::ComPtr<ID3DBlob> errmsg;
-    result = D3DCompileFromFile(
-        L"../../../../projectY/res/shaders/pixel.hlsl",
+    auto result = D3DCompileFromFile(
+        L"../../../../../projectY/res/shaders/pixel.hlsl",
         nullptr,
         nullptr,
         "main",
@@ -208,63 +168,100 @@ int WINAPI WinMain(_In_     HINSTANCE hInstance,
         &errmsg
     );
 
-    BREAK_ON_FAIL(result);
+    GFX_THROW_INFO(pDevice->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &pPixelShader));
 
-    if (errmsg)
-    {
-        const char* msg = (const char *)errmsg->GetBufferPointer();
-        MessageBox(NULL, msg, msg, 0);
-    }
+    // bind pixel shader
+    pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
 
-    Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
-    result = device->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &pixelShader);
-    BREAK_ON_FAIL(result);
+    /////////////////////////////////
+    //Create and bind Vertex Shader//
+    /////////////////////////////////
+    Microsoft::WRL::ComPtr<ID3D11VertexShader> pVertexShader;
 
     result = D3DCompileFromFile(
-        L"../../../../projectY/res/shaders/vertex.hlsl", 
-        nullptr, 
-        nullptr, 
-        "main", 
-        "vs_5_0", 
-        D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 
-        0, 
-        &blob, 
+        L"../../../../../projectY/res/shaders/vertex.hlsl",
+        nullptr,
+        nullptr,
+        "main",
+        "vs_5_0",
+        D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION,
+        0,
+        &blob,
         &errmsg);
 
-    BREAK_ON_FAIL(result);
+    GFX_THROW_INFO(pDevice->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &pVertexShader));
 
-    if (errmsg)
+    // bind vertex shader
+    pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+
+    ////////////////////////////////////////////
+    //Describe the layout of the vertex buffer//
+    ////////////////////////////////////////////
+    Microsoft::WRL::ComPtr<ID3D11InputLayout> pInputLayout;
+    const D3D11_INPUT_ELEMENT_DESC ied[] =
     {
-        const char* msg = (const char *)errmsg->GetBufferPointer();
-        MessageBox(NULL, msg, msg, 0);
-    }
+        { "Position",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+        { "Color",0,DXGI_FORMAT_R32G32B32_FLOAT,0,8u,D3D11_INPUT_PER_VERTEX_DATA,0 },
+    };
+    GFX_THROW_INFO(pDevice->CreateInputLayout(
+        ied, (UINT)std::size(ied),
+        blob->GetBufferPointer(),
+        blob->GetBufferSize(),
+        &pInputLayout
+    ));
 
-    Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
-    result = device->CreateInputLayout(dataDescription, 2, blob->GetBufferPointer(), blob->GetBufferSize(), &inputLayout);
-    BREAK_ON_FAIL(result);
+    // bind vertex layout
+    pContext->IASetInputLayout(pInputLayout.Get());
 
-    Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
-    result = device->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &vertexShader);
-    BREAK_ON_FAIL(result);
+    // bind render target
+    pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), nullptr);
 
-    context->VSSetShader(vertexShader.Get(), nullptr, 0);
-    context->PSSetShader(pixelShader.Get(), nullptr, 0);
-    context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    context->IASetInputLayout(inputLayout.Get());
+    // Set primitive topology to triangle list (groups of 3 vertices)
+    pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-    constexpr FLOAT color[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+    //////////////////////
+    //configure viewport//
+    //////////////////////
+    D3D11_VIEWPORT vp;
+    vp.Width = 800;
+    vp.Height = 600;
+    vp.MinDepth = 0;
+    vp.MaxDepth = 1;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    pContext->RSSetViewports(1u, &vp);
 
-    assert(renderView);
-
-    while (window.open())
+    MSG msg;
+    while (true)
     {
-        window.update();
-        context->ClearRenderTargetView(renderView, color);
-        context->Draw(3u, 0);
-        swapchain->Present(1u, 0u);
-        //OutputDebugString("debug info\n");
-    }
+        while (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
 
-    return 0;
+        //clearBuffer
+        const float color[] = { red,green,blue,1.0f };
+        pContext->ClearRenderTargetView(pTarget.Get(), color);
+
+        pContext->Draw((UINT)std::size(vertices), 0u);
+
+        //endFrame
+        pSwap->Present(1u, 0u);
+    }
 }
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }break;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
 
--- projectY/res/shaders/vertex.hlsl
+++ projectY/res/shaders/vertex.hlsl
@@ -1,4 +1,13 @@
-float4 main(float4 position : POSITION, float3 color : COLOR) : SV_Position
+struct PixelInfo
 {
-    return position;
+    float4 position : SV_Position;
+    float3 color : Color;
+};
+
+PixelInfo main(float2 position : Position, float3 color : Color) 
+{
+    PixelInfo pi;
+    pi.position = float4(position, 1.0, 1.0);
+    pi.color = color;
+    return pi;
 }
