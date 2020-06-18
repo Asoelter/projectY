db9a3d6893db57cfcb68c14cdbee0bbc9e20a3d3
Adam <adamdlsoelter@gmail.com>
2020-06-17 22:32:24 -0500

    added subclasses to token.h

--- projectY/yml/token.h
+++ projectY/yml/token.h
@@ -10,4 +10,27 @@ public:
     virtual std::string toString() const = 0;
 };
 
+class StringToken : public Token
+{
+public:
+    StringToken(const std::string& value);
+
+    std::string toString() const override;
+
+private:
+    std::string value_;
+};
+
+class NumericToken : public Token
+{
+public:
+    NumericToken(double value);
+
+    std::string toString() const override;
+    double value() const;
+
+private:
+    double value_;
+};
+
 #endif// TOKEN_H
